d5d56658d85e2b9c4bedb315810802130a51d772
DS <DS>
2025-09-06 21:09:56 +0200

    Fix: define seq1 and seq2 before use

--- life_breaks_scrub.js
+++ life_breaks_scrub.js
@@ -3,6 +3,10 @@
 
 setcpm(160/4)
 samples('github:tesspilot/samples')
+
+let seq1 = "< 4@3 4@5 4@3 4@1 3@2 6@2 >*8" // straight ahead
+let seq2 = "< 0@3 <0 3>@5 2@3 2@3 4@2 >*8" // alt slices every2 bars
+
 // Using individual sample keys (life1, life3, life5) as defined in strudel.json
 let break1 = s("life1").fit().scrub(seq1.div(8))
 let break2 = s("life3").fit().scrub(seq2.div(8))
